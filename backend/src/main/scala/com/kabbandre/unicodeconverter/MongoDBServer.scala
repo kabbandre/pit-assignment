0f194206ef4902e10d6ab20f149eae5fbb18fb65
kabbandre <kabbandre@gmail.com>
2021-05-26 07:33:51 +0300

    changes

--- backend/src/main/scala/com/kabbandre/unicodeconverter/MongoDBServer.scala
+++ backend/src/main/scala/com/kabbandre/unicodeconverter/MongoDBServer.scala
@@ -1,12 +1,63 @@
 package com.kabbandre.unicodeconverter
 
 import org.mongodb.scala._
-import org.bson._
+import org.bson.types.ObjectId
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import org.bson.BsonValue
 
 object MongoDBServer {
   val uri: String = "mongodb+srv://admin:admin@cluster0.jpaeu.mongodb.net/UnicodeConverter?retryWrites=true&w=majority"
+  System.setProperty("org.mongodb.async.type", "netty")
   val client: MongoClient = MongoClient(uri)
   val db: MongoDatabase = client.getDatabase("UnicodeConverter")
-  val filters = db.getCollection("Filters")
-  val pictures = db.getCollection("Pictures")
+  
+  val imagesCollection: MongoCollection[Document] = db.getCollection("images")
+  val filtersCollection: MongoCollection[Document] = db.getCollection("filters")
+
+  def generateAscii(): String = {
+    return "Suka"
+  }
+  
+  def addImage(image: Image): BsonValue = {    
+    val doc: Document = Document(
+      "_id" -> 0,
+      "title" -> image.title,
+      "width" -> image.width,
+      "filterId" -> image.filterId,
+      "image" -> image.image,
+      "createdAt" -> image.createdAt,
+      "processedImage" -> image.processedImage
+    )
+
+    val e = imagesCollection.insertOne(doc)
+    val r = Await.result(e.toFuture(), Duration.Inf)
+
+    println(r)
+    r.getInsertedId()
+  }
+
+  def getImage(id: ObjectId): Document = {
+    val e = imagesCollection.find(Document("_id" -> id)).first()
+    val r = Await.result(e.toFuture(), Duration.Inf)
+
+    println(r)
+    r
+  }
+
+  def getImages(): Seq[Document] = {
+    val e = imagesCollection.find()
+    val r = Await.result(e.toFuture(), Duration.Inf)
+
+    println(r)
+    r
+  }
+
+  def getFilters(): Seq[Document] = {
+    val e = filtersCollection.find()
+    val r = Await.result(e.toFuture(), Duration.Inf)
+
+    println(r)
+    r
+  }
 }
--- backend/src/main/scala/com/kabbandre/unicodeconverter/UnicodeConverter.scala
+++ backend/src/main/scala/com/kabbandre/unicodeconverter/UnicodeConverter.scala
@@ -6,24 +6,81 @@ import org.http4s.circe._
 import org.http4s.server._
 import org.http4s.dsl._
 import scalaz.concurrent.Task
+import org.bson.types._
+import scalaz.effect.IO
+import org.bson.{BsonValue, BsonBinary, BsonDateTime}
+import org.mongodb.scala._
+import org.mongodb.scala.bson.collection.mutable
 
-object UnicodeConverter {
+object ImageModels {
+  type ImageId = ObjectId
+  type Width = Int  
+  type ImageFile = BsonBinary
+  type CreatedAt = BsonDateTime
+  type ProcessedImage = String
+  type Title = String
+}
+
+object FilterModels {
+  type FilterId = ObjectId
+  type FilterName = String
+}
+
+case class Image (title: ImageModels.Title, width: ImageModels.Width, filterId: FilterModels.FilterId, image: ImageModels.ImageFile, createdAt: ImageModels.CreatedAt, processedImage: ImageModels.ProcessedImage)
+case class ImageWithId (title: ImageModels.Title, id: ImageModels.ImageId, width: ImageModels.Width, filterId: FilterModels.FilterId, image: ImageModels.ImageFile, createdAt: ImageModels.CreatedAt, processedImage: ImageModels.ProcessedImage)
+case class Filter (filterId: FilterModels.FilterId, name: FilterModels.FilterName)
+
+trait UnicodeConverterRepo {
+  def generateAscii(): String
+  def addImage(image: Image): BsonValue
+  def getImage(id: ImageModels.ImageId): Document
+  def getImages(): Seq[Document]
+  def getFilters(): Seq[Document]
+}
+
+object Impl extends UnicodeConverterRepo {    
+  override def generateAscii(): String = MongoDBServer.generateAscii()
+  override def addImage(image: Image): BsonValue = MongoDBServer.addImage(image)
+  override def getImage(id: ImageModels.ImageId) = MongoDBServer.getImage(id)
+  override def getImages() = MongoDBServer.getImages()
+  override def getFilters() = MongoDBServer.getFilters()
+}
+
+object UnicodeConverter {  
+  import Impl._
+
+  def jsonizeDocs(cDocuments: Seq[Document]): String = {
+    val sb = new StringBuilder
+    for (doc <- cDocuments) {
+      if (sb.nonEmpty) {
+        sb.append(",")
+      }
+      sb.append(doc.toJson)
+    }
+    sb.toString
+  }
+  def jsonizeDoc(cDocument: Document): String = {
+    val sb = new StringBuilder
+    sb.append(cDocument.toJson)
+    sb.toString
+  }
+
   def generateImage (req: Request): Task[Response] = {
-    Ok("Generate")
+    Ok(generateAscii())
   }
   def saveImage (req: Request): Task[Response] = {
     Ok("Save")
   }
   def getAllImages (): Task[Response] = {
-    Ok("Get all images")
+    Ok(jsonizeDocs(getImages()))
   }
-  def getOneImage (id: String): Task[Response] = {
-    Ok("get image " + id)
+  def getOneImage (id: ImageModels.ImageId): Task[Response] = {
+    Ok(jsonizeDoc(getImage(id)))
   }
   val service = HttpService {
     case req @ POST -> Root / "generate" => generateImage(req)
     case req @ POST -> Root / "save" => saveImage(req)
     case GET -> Root => getAllImages
-    case GET -> Root / id => getOneImage(id)
+    case GET -> Root / id => getOneImage(new ObjectId(id))
   }
 }
